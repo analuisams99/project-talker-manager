53d5d8e2ff33ee25393c2bc73231f466b1cef30e
Ana Luisa <ana.luisa.isa@hotmail.com>
2022-01-25 18:36:15 -0300

    Req 5 - Cria o endpoint PUT/talker/:id

--- index.js
+++ index.js
@@ -1,12 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const app = express();
-app.use(bodyParser.json());
-
-const HTTP_OK_STATUS = 200;
-const PORT = '3000';
-
 const talker = require('./middlewares/talker');
 const talkerId = require('./middlewares/talkerId');
 const { loginToken, emailValidation, passwordValidation } = require('./middlewares/login');
@@ -18,7 +12,14 @@ const {
   rateValidation,
   talkValidation,
 } = require('./middlewares/talkerValidation');
-const { addTalker } = require('./middlewares/addTalker');
+const addTalker = require('./middlewares/addTalker');
+const editTalker = require('./middlewares/editTalker');
+
+const app = express();
+app.use(bodyParser.json());
+
+const HTTP_OK_STATUS = 200;
+const PORT = '3000';
 
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
@@ -37,6 +38,16 @@ app.post(
   dateValidation,
   rateValidation,
   addTalker,
+);
+app.put(
+  '/talker/:id',
+  tokenValidation,
+  nameValidation,
+  ageValidation,
+  talkValidation,
+  dateValidation,
+  rateValidation,
+  editTalker,
 );
 
 app.listen(PORT, () => {
--- middlewares/editTalker.js
+++ middlewares/editTalker.js
@@ -1,20 +1,17 @@
 const fs = require('fs').promises;
 
 module.exports = async (req, res) => {
+  const { id } = req.params;
   const { name, age, talk } = req.body;
 
   const talkers = await fs.readFile('./talker.json', 'utf-8')
   .then((response) => JSON.parse(response));
 
-  const newTalker = {
-    id: talkers.length + 1,
-    name,
-    age,
-    talk,
-  };
+  const findId = talkers.findIndex((talker) => talker.id === +id);
 
-  talkers.push(newTalker);
-
+  talkers[findId] = { ...talkers[findId], name, age, talk };
+  
   await fs.writeFile('./talker.json', JSON.stringify(talkers));
-  res.status(201).json(newTalker);
+
+  res.status(200).json(talkers[findId]);
 };
